c840d304d553ce98356baed9c02c0abe5af15143
StartShark <206740779@qq.com>
2025-02-08 17:41:32 +0800

    Style and API fixed.

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,36 +1,36 @@
-public class ArrayDeque<T>{
+public class ArrayDeque<T> {
     private T[] items;
     private int size;
     private int front;
     private int back;
 
-    public ArrayDeque(){
+    public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
         front = 0;
         back = 0;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public void resize(){
+    private void resize() {
         /** Not quite right */
         int len = items.length;
-        if(size == len){
+        if (size == len) {
             T[] p = (T[]) new Object[2 * len];
             System.arraycopy(items, 0, p, 0, back + 1);
-            if(front != 0){
+            if (front != 0) {
                 System.arraycopy(items, front, p, len + front, len - front);
                 front = len + front;
             }
             items = p;
         }
-        else if(len >= 16 && size < len / 4){
+        else if (len >= 16 && size < len / 4) {
             T[] p = (T[]) new Object[len / 2];
             System.arraycopy(items, 0, p, 0, back + 1);
-            if(front != 0){
+            if (front != 0) {
                 System.arraycopy(items, front, p, front - len / 2, len - front);
                 front = front - len / 2;
             }
@@ -38,41 +38,41 @@ public class ArrayDeque<T>{
         }
     }
 
-    private int plus(int num){
+    private int plus(int num) {
         return (num == items.length - 1) ? 0 : num + 1;
     }
 
-    private int minus(int num){
+    private int minus(int num) {
         return (num == 0) ? items.length - 1 : num - 1;
     }
 
-    public void addFirst(T item){
+    public void addFirst(T item) {
         resize();
-        while(items[front] != null){
+        while (items[front] != null) {
             front = minus(front);
         }
         items[front] = item;
         size++;
     }
 
-    public void addLast(T item){
+    public void addLast(T item) {
         resize();
-        while(items[back] != null){
+        while (items[back] != null) {
             back = plus(back);
         }
         items[back] = item;
         size++;
     }
 
-    public T removeFirst(){
-        if(size <= 0) return null;
+    public T removeFirst() {
+        if (size <= 0) return null;
 
         T returnItem = items[front];
         items[front] = null;
         front = plus(front);
         size--;
         
-        if(size == 0){
+        if (size == 0) {
             front = 0;
             back = 0;
         }
@@ -81,15 +81,15 @@ public class ArrayDeque<T>{
         return returnItem;
     }
 
-    public T removeLast(){
-        if(size <= 0) return null;
+    public T removeLast() {
+        if (size <= 0) return null;
         
         T returnItem = items[back];
         items[back] = null;
         back = minus(back);
         size--;
 
-        if(size == 0){
+        if (size == 0) {
             front = 0;
             back = 0;
         }
@@ -98,25 +98,25 @@ public class ArrayDeque<T>{
         return returnItem;
     }
     
-    public void printDeque(){
+    public void printDeque() {
         int f = front, b = back;
-        if(f == b) System.out.println();
-        while(f != b){
+        if (f == b) System.out.println();
+        while (f != b) {
             System.out.print(items[f] + " ");
             f = plus(f);
-            if(f == b) System.out.println(items[f]);
+            if (f == b) System.out.println(items[f]);
         }
         System.out.println();
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public T get(int index){
+    public T get(int index) {
         int len = items.length;        
         int actual = (front + index >= len) ? front + index - len : front + index;
-        if(index < 0 || actual >= len){
+        if (index < 0 || actual >= len) {
             return null;
         }
         else return items[actual];
